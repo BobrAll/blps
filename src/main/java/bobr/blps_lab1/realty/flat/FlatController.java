4940bbb8c5b543a3e358b6c374315fabbdc428f2
bobr <bobrus91@gmail.com>
2024-02-07 13:28:16 +0300

    Done: Added flat search filters

--- src/main/java/bobr/blps_lab1/realty/flat/FlatController.java
+++ src/main/java/bobr/blps_lab1/realty/flat/FlatController.java
@@ -3,6 +3,7 @@ package bobr.blps_lab1.realty.flat;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -10,10 +11,29 @@ import java.util.List;
 @RequestMapping("/api/v1/flats")
 public class FlatController {
     private final FlatService flatService;
-
+    //TODO remake filters (to custom sql)
     @GetMapping
-    public List<Flat> getFlats() {
-        return flatService.findAll();
+    public List<Flat> getFlats(@RequestBody FlatCriteria filters) {
+        return flatService
+                .findAll()
+                .stream()
+                .filter(flat ->
+                        (filters.getMaxTotalArea() == null || flat.getTotalArea() < filters.getMaxTotalArea())
+                                && (filters.getMinTotalArea() == null || flat.getTotalArea() > filters.getMinTotalArea())
+
+                                && (filters.getMaxLivingArea() == null || flat.getLivingArea() < filters.getMaxLivingArea())
+                                && (filters.getMinLivingArea() == null || flat.getLivingArea() > filters.getMinLivingArea())
+
+                                && (filters.getMaxKitchenArea() == null || flat.getKitchenArea() < filters.getMaxKitchenArea())
+                                && (filters.getMinKitchenArea() == null || flat.getKitchenArea() > filters.getMinKitchenArea())
+
+                                && (filters.getHaveBalcony() == null || flat.getHaveBalcony() == filters.getHaveBalcony())
+                                && (filters.getIsApartments() == null || flat.getIsApartments() == filters.getIsApartments())
+
+                                && (filters.getFloor() == null || flat.getFloor().equals(filters.getFloor()))
+                                && (filters.getRooms() == null || Arrays.binarySearch(filters.getRooms(), flat.getRooms()) >= 0)
+                )
+                .toList();
     }
 
     @PostMapping()
