c77c6ba0632b1a09b8cc7ea4751ca6ced92efb96
bobr <bobrus91@gmail.com>
2024-02-12 21:41:36 +0300

    Beautify sql query

--- src/main/java/bobr/blps_lab1/realty/flat/FlatRepository.java
+++ src/main/java/bobr/blps_lab1/realty/flat/FlatRepository.java
@@ -2,33 +2,32 @@ package bobr.blps_lab1.realty.flat;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface FlatRepository extends JpaRepository<Flat, Integer> {
     @Query("SELECT f FROM Flat f " +
-            "WHERE (:floor is null or f.floor = :floor) " +
-            "AND (:rooms is null or f.rooms in :rooms) " +
-            "AND (:haveBalcony is null or f.haveBalcony = :haveBalcony) " +
-            "AND (:isApartments is null or f.isApartments = :isApartments) " +
-            "AND (:minTotalArea is null or f.totalArea >= :minTotalArea) " +
-            "AND (:maxTotalArea is null or f.totalArea <= :maxTotalArea) " +
-            "AND (:minKitchenArea is null or f.kitchenArea >= :minKitchenArea) " +
-            "AND (:maxKitchenArea is null or f.kitchenArea <= :maxKitchenArea) " +
-            "AND (:minLivingArea is null or f.livingArea >= :minLivingArea) " +
-            "AND (:maxLivingArea is null or f.livingArea <= :maxLivingArea)"
+            "WHERE (:floor IS NULL OR f.floor = :floor) " +
+            "AND (:rooms IS NULL OR f.rooms IN :rooms) " +
+            "AND (:haveBalcony IS NULL OR f.haveBalcony = :haveBalcony) " +
+            "AND (:isApartments IS NULL OR f.isApartments = :isApartments) " +
+            "AND (:minTotalArea IS NULL OR f.totalArea >= :minTotalArea) " +
+            "AND (:maxTotalArea IS NULL OR f.totalArea <= :maxTotalArea) " +
+            "AND (:minKitchenArea IS NULL OR f.kitchenArea >= :minKitchenArea) " +
+            "AND (:maxKitchenArea IS NULL OR f.kitchenArea <= :maxKitchenArea) " +
+            "AND (:minLivingArea IS NULL OR f.livingArea >= :minLivingArea) " +
+            "AND (:maxLivingArea IS NULL OR f.livingArea <= :maxLivingArea)"
     )
     List<Flat> findWithFilters(
-            @Param("minTotalArea") Double minTotalArea,
-            @Param("maxTotalArea") Double maxTotalArea,
-            @Param("minKitchenArea") Double minKitchenArea,
-            @Param("maxKitchenArea") Double maxKitchenArea,
-            @Param("minLivingArea") Double minLivingArea,
-            @Param("maxLivingArea") Double maxLivingArea,
-            @Param("rooms") Integer[] rooms,
-            @Param("floor") Integer floor,
-            @Param("haveBalcony") Boolean haveBalcony,
-            @Param("isApartments") Boolean isApartments
+            Double minTotalArea,
+            Double maxTotalArea,
+            Double minKitchenArea,
+            Double maxKitchenArea,
+            Double minLivingArea,
+            Double maxLivingArea,
+            Integer[] rooms,
+            Integer floor,
+            Boolean haveBalcony,
+            Boolean isApartments
             );
 }
